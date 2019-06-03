8a6c6bc264713507b05ced9723af3912a364dfd5
rapu786 <rhemanth20@gmail.com>
2019-06-03 10:00:31 +0530

    Adds solution to 20.Categories, and the total products in each category to intermediateProblems.sql

--- intermediateProblems.sql
+++ intermediateProblems.sql
@@ -1,2 +1,13 @@
 Use Northwind_SPP;
 GO
+
+-- 20.Categories, and the total products in each category
+-- For this probem, we'd like to see the total number of products in each category
+-- Sort the results by the total numbers of products, in descending order
+
+SELECT a.CategoryName, Count(b.CategoryID) AS TotalProducts
+FROM Categories a
+Inner JOIN Products b
+ON a.CategoryID = b.CategoryID
+GROUP BY a.CategoryName
+ORDER BY TotalProducts DESC;
