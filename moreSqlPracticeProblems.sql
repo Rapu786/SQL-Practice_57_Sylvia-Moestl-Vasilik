d38f183ad17b8faf5353e95e6ac09453b034a42c
rapu786 <rhemanth20@gmail.com>
2019-06-10 06:39:37 +0530

    Added solution to 2.Customers with total orders placed in moreSqlPracticeProblems

--- moreSqlPracticeProblems.sql
+++ moreSqlPracticeProblems.sql
@@ -6,3 +6,12 @@ COUNT(*) AS TotalPriceChanges
 FROM ProductCostHistory
 GROUP BY ProductID
 ORDER BY ProductID;
+
+--2.Customers with total orders placed
+
+SELECT 
+CustomerID,
+COUNT(*) AS TotalOrders
+FROM SalesOrderHeader
+GROUP BY CustomerID
+ORDER BY TotalOrders DESC;
