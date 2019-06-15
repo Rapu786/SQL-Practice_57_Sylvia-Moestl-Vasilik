610bc928e8b12f3e86def6c913785118198fd2d7
rapu786 <rhemanth20@gmail.com>
2019-06-15 12:48:48 +0530

    added solution to '4.Products with first and last order date, including name' in moreSqlPracticeProblems.sql

--- moreSqlPracticeProblems.sql
+++ moreSqlPracticeProblems.sql
@@ -25,3 +25,12 @@ ON a.SalesOrderID = b.SalesOrderID
 GROUP BY a.ProductID
 ORDER BY ProductID;
 
+--4.Products with first and last order date, including name
+SELECT a.ProductID, c.ProductName ,MIN(CAST(b.OrderDate AS DATE)) AS FirstOrder , Max(CAST(b.OrderDate AS DATE)) AS LastOrder
+FROM SalesOrderDetail a
+Inner Join SalesOrderHeader b
+ON a.SalesOrderID = b.SalesOrderID
+Inner Join Product c
+On a.ProductID = c.ProductID
+GROUP BY a.ProductID, c.ProductName
+ORDER BY ProductID;
